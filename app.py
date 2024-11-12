cf76ec84ee6ec89c133c2ef3ac2f70cc80647d88
Rachid <rachidjmine95@gmail.com>
2024-11-12 18:44:01 +0100

    Added CORS to app.py

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ from google.oauth2 import service_account
 from google.cloud import vision
 
 app = Flask(__name__)
-# Permitir todas las solicitudes y orígenes
-CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+CORS(app, resources={r"/analizar": {"origins": "*"}})
 
 # Cargar credenciales desde la variable de entorno
 credenciales_json = os.getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON")
